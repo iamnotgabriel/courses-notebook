a445a086cc27515afa82df57214e4e877e23a87e
Gabriel Borges <gabrielfr.borges@gmail.com>
2023-03-14 19:10:01 -0300

    add spring project

---
+++ spring/indepth_spring/src/main/java/com/learn_spring/indepth_spring/IndepthSpringApplication.java
@@ -0,0 +1,13 @@
+package com.learn_spring.indepth_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndepthSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndepthSpringApplication.class, args);
+	}
+
+}
---
+++ spring/indepth_spring/src/test/java/com/learn_spring/indepth_spring/IndepthSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn_spring.indepth_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndepthSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
